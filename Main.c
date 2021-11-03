23744afce65abe567ea561fc9bb5b8a8db23351e
Julenissen <Henrika2001@hotmail.com>
2021-11-03 20:51:11 +0100

    Delete operational, quitApp startet +bugfixes
    Delete funker nå etter intensjon, måtte bare resete hvilket rewind() funksjonen løste. quitApp ser tilsynelatende ferdig ut, men krasjer på input, må feilsøkes. Diverse bugfixes: main() er nå i loop, og "jump:" er plassert mer taktisk. Databasen printer nå estetisk vakrere. Mulig man må tenke litt mer kreativt før man påbegynner editUser funksjonen, da vi akkurat nå kun har passord og epost og edite. Dette er isåfall en avgjørelse som vil føre til endringer i hele koden. Umiddelbare tanke er to separate databaser, kan mulig også løses ved en intelligent struct

--- Main.c
+++ Main.c
@@ -5,27 +5,31 @@
     void RegisterUser ();
    // int Crypto(char Password[50]);
     void UserDelete ();
-    /* void UserEdit ();
-    void applicationQuit(); */
+    // void UserEdit ();
+    int applicationQuit(); 
 
 
 int main() {
-    int k;
+    int k, path;
+    int a = 1;
     printf("Welcome to GreenhatAI \n \n \n \n \n \n \n");
+    jump:
     printf("Choose your course of action \n");
     printf("Press 1 for new user \n");
     printf("Press 2 to edit already existing user \n");
     printf("Press 3 to delete your account \n");
     printf("Press 9 to quit application \n");
-    jump: 
+    while (a==1){ 
         scanf("%d",&k);
         switch (k) {
             case 1: 
                 RegisterUser();
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n New user successfully registered \n");
+                goto jump;
                 break;
             case 2:
                 //UserEdit ();
-                printf("Error, Path not yet coded \n");
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n Error, Path not yet coded \n");
                 goto jump;
                 break;
             case 3:
@@ -33,10 +37,19 @@ int main() {
                 goto jump;
                 break;
             case 9:
-               // applicationQuit();
-                printf("Error, Path not yet coded \n");
-                goto jump;
-                break; 
+                Top:
+                    path = applicationQuit();
+                    printf("%i", path);
+                    if (path == 1){
+                        goto Exit;
+                    }
+                    if (path == 2) {
+                        goto jump; 
+                    }
+                    else {
+                        printf("\n Invalid input, try again");
+                        goto Top;
+                    }
             default:
                 printf("\n Invalid input \n");
                 printf("Press 1 for new user \n");
@@ -46,6 +59,8 @@ int main() {
                 goto jump;
                 break;
     }
+    }
+    Exit:
     return 0;
 
 }
@@ -61,8 +76,8 @@ void RegisterUser () {
     printf("\n \n \n \n \n \n \n \n \n \n Email: %s \n \n Please select a password: \n", Email);
     scanf("%s", Password);
     Database = fopen("Fortrolig Database", "a+");
-    fprintf(Database,"\n %s", Email);
-    fprintf(Database,"\n %s", Password);
+    fprintf(Database,"\n%s", Email);
+    fprintf(Database,"\n%s", Password);
     fclose(Database);
     /* char CribbedPassword = Crypto(Password);
      StorinUnit(Email, CribbedPassword); */
@@ -81,7 +96,7 @@ void UserDelete () {
     char Password[50];
     char fill [50];
     char fill2 [50];
-    int count = 0;
+    int count = 1;
     int line = 0;
     char fill3 [50];
     printf("Enter your Email: \n");
@@ -93,9 +108,10 @@ void UserDelete () {
     while ((fscanf(Database,"%s", fill) == 1) && (fscanf(Database,"%s", fill2) == 1)) {
         count ++;
         if((strstr(fill, Email)!=0) && (strstr(fill2, Password)!=0)){
+            rewind(Database);
             while (fgets(fill3, 50, Database)!= NULL){      //while loop doesnt start at top, but at found value
                line ++;
-               if (count != line){
+               if ((count != line) &&  (count+1 != line)) {
                     printf("%s", fill3);
                     fprintf(Database2, fill3);
                } 
@@ -110,6 +126,21 @@ void UserDelete () {
         remove("Fortrolig Database");
         rename("Midlertidig Database", "Fortrolig Database");
     }
+    else {
+        fclose(Database2);
+        fclose(Database);
+        remove("Midlertidig Database");
+    }
 } 
+
+int applicationQuit() {   //Denne funksjonen krasjer på input 4 some unknown reason
+    int answer;
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n Are you sure you want to quit? \n");
+    printf("Press 1 for yes, and 2 for no \n");
+    scanf("%i", answer);
+    return answer;
+    }
+
     
 
+    
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # User-Management-System-in-C
 School project, intro course to C programming
+
+Mårvad alertness-test, skriv "ja" på streken med gjeldende dato og tidspunkt når melding lest
+
+
+
+
+
+.......................................
